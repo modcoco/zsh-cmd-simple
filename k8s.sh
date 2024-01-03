0723ae3a7d37b9b1cd97688cb5d3c35db4b26005
modcoco <codepapercut@gmail.com>
2024-01-03 11:37:55 +0800

    Add enter k8s and exec cmd

--- k8s.sh
+++ k8s.sh
@@ -3,3 +3,5 @@ kubectl describe pod <pod_name> | grep -E "Container|Name"
 
 # Enter in Multi container pod's child pod
 kubectl exec -it pod <pod_name> -c <sub_pod_name> -- /bin/bash
+
+kep <pod_name> <container_name> <psql -h localhost -U postgres>
